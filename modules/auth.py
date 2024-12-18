bd1216a60d125943e958d0cd49650a7f65bb42ae
Stevesuk <121714313+Stevesuk0@users.noreply.github.com>
2024-12-18 17:24:01 +0800

    Update auth.py
    typo

--- modules/auth.py
+++ modules/auth.py
@@ -3,7 +3,7 @@ import os
 
 
 def run_as_root(args):
-    os.system(f"sudo sh -c 'cd {os.getcwd()}; /bin/python3 {args}'") 
+    os.system(f"sudo sh -c 'cd {os.getcwd()}; /bin/python3 launcher.py {args}'") 
 
-def check_is_root(): # return true when user is root
+def checkIsRoot(): # return true when user is root
     return os.getuid() == 0 
